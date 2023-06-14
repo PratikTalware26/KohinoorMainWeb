245fbf9530e4c17088ee2d3fcf1a748c4cac6154
pratik.talware <brickfolio_admin@Apples-MacBook-Air.local>
2023-06-14 17:27:14 +0530

    created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import './App.css'
+import AllHomeComponents from './Components/AllHomeComponents'
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      Hello
+      <AllHomeComponents/>
     </div>
   )
 }
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "./Navbar.css"
+import mainLogo from "../assets/Kohinoor new logo for website-01 (1)-2.webp"
+
+const Navbar = () => {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg navbar-light">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="#">
+            <img src={mainLogo} alt="" />
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link active text-light" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-light" href="#">
+                  Features
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link text-light" href="#">
+                  Pricing
+                </a>
+              </li>
+              <li className="nav-item">
+                <button className="nav-link btn">SALES ENQUIRY</button>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
